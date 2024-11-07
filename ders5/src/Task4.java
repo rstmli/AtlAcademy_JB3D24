2f8859ba61f686bf8dfb8b91435dd4c9b9871d17
Huseyn Rustemli <rstmli08@gmail.com>
2024-11-08 00:38:33 +0400

    newTask

--- ders5/src/Task4.java
+++ ders5/src/Task4.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir ay daxil edin: ");
         var mount = scanner.next();
         switch(mount){
             case "dekabr":
@@ -14,14 +15,19 @@ public class Task4 {
             case "aprel":
             case "may":
                 System.out.println("yaz fesli");
+                break;
             case "iyun":
             case "iyul":
             case "avqust":
                 System.out.println("yay fesli");
+                break;
             case "sentyabr":
             case "oktyabr":
             case "noyabr":
                 System.out.println("payız fesli");
+                break;
+            default:
+                System.out.println("bele bir ay yaxdur");
         }
     }
 }
