8e542540a8b1d98fa5a2354041c1e5d4b72337d3
Huseyn Rustemli <rstmli08@gmail.com>
2024-11-07 21:03:17 +0400

    ATLACADEMY

--- ders5/src/Task1.java
+++ ders5/src/Task1.java
@@ -7,12 +7,11 @@ public class Task1 {
         var num = scanner.nextInt();
         if(num%3==0 && num%4==0){
             System.out.println("Bu ədəd həm 3'ə həm 4'ə qalıqsız bölünür.");
-        }
-        else if(num%3==0) {
+        } else if(num%3==0) {
             System.out.println("Bu ədəd Yalnız 3'ə qalıqsız bölünür.");
-        }else if(num%4==0) {
+        } else if(num%4==0) {
             System.out.println("Bu ədəd yalnız 4'ə qalıqsız bölünür.");
-        }else{
+        } else{
             System.out.println("Bu ədəd 3'ə və 4'ə qalıqsız bölünmür.");
         }
     }
