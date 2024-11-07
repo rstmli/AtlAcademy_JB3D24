73c52b4e0da0f4ed4cf72b6d1cd713ad594355f5
Huseyn Rustemli <rstmli08@gmail.com>
2024-11-07 20:57:53 +0400

    3'e ve ya 4'e qaliqsiz bolunme alqoritmi

---
+++ ders5/src/Task1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ədədi daxil edin: ");
+        var num = scanner.nextInt();
+        if(num%3==0 && num%4==0){
+            System.out.println("Bu ədəd həm 3'ə həm 4'ə qalıqsız bölünür.");
+        }
+        else if(num%3==0) {
+            System.out.println("Bu ədəd Yalnız 3'ə qalıqsız bölünür.");
+        }else if(num%4==0) {
+            System.out.println("Bu ədəd yalnız 4'ə qalıqsız bölünür.");
+        }else{
+            System.out.println("Bu ədəd 3'ə və 4'ə qalıqsız bölünmür.");
+        }
+    }
+}
