74b4ea3ba124a709a5e0bc73ef104eba24016668
Huseyn Rustemli <rstmli08@gmail.com>
2024-11-13 23:42:46 +0400

    newTask

--- ders4/src/Task2.java
+++ ders4/src/Task2.java
@@ -5,12 +5,8 @@ public class Task2 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ədədi daxil edin: ");
         var num = scanner.nextInt();
-        if(num%3==0 && num%4==0){
-            System.out.println("Bu ədəd həm 3'ə həm 4'ə qalıqsız bölünür.");
-        } else if(num%3==0) {
-            System.out.println("Bu ədəd Yalnız 3'ə qalıqsız bölünür.");
-        } else if(num%4==0) {
-            System.out.println("Bu ədəd yalnız 4'ə qalıqsız bölünür.");
+        if(num%3==0 || num%4==0) {
+            System.out.println("Bu ədəd həm 3'ə və ya 4'ə qalıqsız bölünür.");
         } else{
             System.out.println("Bu ədəd 3'ə və 4'ə qalıqsız bölünmür.");
         }
