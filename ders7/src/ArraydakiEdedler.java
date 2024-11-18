1866ff2b8dfd5987fa3b2b388d983a8bdb792a12
Huseyn Rustemli <rstmli08@gmail.com>
2024-11-18 11:47:06 +0400

    newTask

--- ders7/src/ArraydakiEdedler.java
+++ ders7/src/ArraydakiEdedler.java
@@ -5,14 +5,13 @@ public class ArraydakiEdedler {
         Scanner scanner = new Scanner(System.in);
         int[] arr = new int[5];
         int i = 0;
-        int tut = arr[0];
+        int tut = 0;
         System.out.println("↓ 5 ədəd daxil edin ↓");
 
         for(i = 0; i<5; i++){
             arr[i] = scanner.nextInt();
         }
         System.out.print("Arrayın elementleri: ");
-
         for(i = 0;i<5;i++){
             tut++;
             System.out.print(arr[i]);
