75dcd9958c2e891178a5bd4791058fc06fef0bb6
Huseyn Rustemli <rstmli08@gmail.com>
2024-11-21 11:01:51 +0400

    newTask

--- ders7/src/SumAverage.java
+++ ders7/src/SumAverage.java
@@ -6,13 +6,13 @@ public class SumAverage {
         int[] arr = new int[5];
         int i = 0;
         double average = 0.0;
-        for(i = 0;i<5;i++){
+        for(i = 0;i<arr.length;i++){
             arr[i] = scanner.nextInt();
             average = average+arr[i];
         }
 
         int sum = (int) average;
-        System.out.println("ədədlərin cəmi: " + sum + " ədədi ortası: " + sum/5);
+        System.out.println("ədədlərin cəmi: " + sum + " ədədi ortası: " + average/5);
 
     }
 }
