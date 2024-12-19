6dc41370d3c7c730e67c6bcd281b811d5c3f5dca
Huseyn Rustemli <rstmli08@gmail.com>
2024-12-19 16:49:30 +0400

    Generic, Predicate and Stream API Task

--- Ders 20/src/GenericTask/Generic.java
+++ Ders 20/src/GenericTask/Generic.java
@@ -4,21 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+https://getintopc.com/softwares/office-tools/microsoft-office-2016-pro-plus-visio-project-64-bit-download-1218814/
 
 public class Generic{
     public static void main(String[] args) {
         List<Double> double2 = Arrays.asList(1.0,2.0,3.4);
+        chapet(double2, e -> e > 2.0);
         List<Integer> integer2 = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        chapet(integer2, e -> e%2==0);
         List<String> string2 = Arrays.asList("Huseyn","Hemid","Nurlu","Nihad","Emin");
-        Predicate<Double> doublePiredicate = e -> e>2.0;
-        Predicate<Integer> intPiredicate = e -> e%2==0;
-        Predicate<String> strPiredicate = e -> e.startsWith("N");
-        System.out.println("2.0 dan boyuk elementler");
-        chapet(double2,doublePiredicate);
-        System.out.println("Cut elementler");
-        chapet(integer2,intPiredicate);
-        System.out.println("adi N ile bashlayan Istifadəçilər");
-        chapet(string2,strPiredicate);
+        chapet(string2,e -> e.startsWith("N"));
 
     }
 
