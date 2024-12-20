cce403db7d57001b5a7a3a5d38171b42e406543f
Huseyn Rustemli <rstmli08@gmail.com>
2024-12-20 13:25:22 +0400

    IntegerStreamTask

--- Ders 20/src/SetTaskStream/IntegerTask2.java
+++ Ders 20/src/SetTaskStream/IntegerTask2.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 
 public class IntegerTask2 {
     public static void main(String[] args) {
-        var a = Arrays.asList(1,2,3,4,5,6,78,9,9,45,34,534,534,534,5346,43,6);
-        var b = a.stream().map(e -> Arrays.asList(e)).toList();
-        System.out.println(b);
+        var a = Arrays.asList(1,2,3,4,5);
+        var b = Arrays.asList(6,7,8,9,10);
+        var c = Arrays.asList(11,12,13,14,15);
+        var d = Arrays.asList(a,b,c);
+        System.out.println(d);
     }
 }
